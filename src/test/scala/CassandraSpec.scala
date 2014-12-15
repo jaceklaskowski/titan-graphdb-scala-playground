91b2053f8722c376d97d622cc07e9ff2b3c4dc0e
Jacek Laskowski <jacek@japila.pl>
2014-12-15 10:51:57 +0100

    Leave Titan empty after tests + introducing After scope

--- src/test/scala/CassandraSpec.scala
+++ src/test/scala/CassandraSpec.scala
@@ -1,5 +1,6 @@
+import com.thinkaurelius.titan.core.util.TitanCleanup
 import com.tinkerpop.gremlin.process.T
-import com.tinkerpop.gremlin.scala.GremlinScala
+import com.tinkerpop.gremlin.scala.{ScalaEdge, ScalaVertex, GremlinScala}
 import org.specs2._
 
 class CassandraSpec extends Specification { def is = s2"""
@@ -7,35 +8,52 @@ class CassandraSpec extends Specification { def is = s2"""
   This is a specification to check Titan+Cassandra setup
 
   Titan+Cassandra should
-    be available to clients   $e1
+    be available to clients   ${graph().e1}
                               """
 
-  def e1 = {
-    import com.thinkaurelius.titan.core.TitanFactory
-    val g = TitanFactory.build()
-      .set("storage.backend", "cassandra")
-      .set("storage.hostname", "127.0.0.1")
-      .open()
+  case class graph() extends specification.After {
+    lazy val (g, gs) = createGraph()
+    def after = {
+      println("\nafter\n")
+      //cleanupDB()
+    }
 
-    val gs = GremlinScala(g)
+    def e1 = {
+      val n0: ScalaVertex = gs.V.has(T.label, "n0").head
+      val e7: ScalaEdge = gs.E.has(T.label, "e7").head
+      val e8: ScalaEdge = gs.E.has(T.label, "e8").head
 
-    val n0  = gs.addVertex(label = "n0",  properties = Map("name" -> "ORac"))
-    val n1  = gs.addVertex(label = "n1",  properties = Map("name" -> "elek"))
-    val n11 = gs.addVertex(label = "n11", properties = Map("name" -> "elek"))
-    val n12 = gs.addVertex(label = "n12", properties = Map("name" -> "elek"))
+      n0.outE("e7").head must_=== e7.edge
+      n0.outE("e8").head must_=== e8.edge
+      n0.out().count().head() must_=== 2.toLong
 
-    val e7 = n0.addEdge("e7", n11, Map.empty)
-    val e8 = n0.addEdge("e8", n12, Map.empty)
-    val e9 = n1.addEdge("e9", n0, Map.empty)
+      gs.V.count().head must_=== 4.toLong
+      gs.close()
 
-    n0.outE.head must_=== e7.edge
-    n0.outE("e8").head must_=== e8.edge
-    n0.out().count().head() must_=== 2.toLong
+      TitanCleanup.clear(g)
 
-    println(gs.V.count().head())
+      success
+    }
 
-    g.close()
+    def createGraph() = {
+      import com.thinkaurelius.titan.core.TitanFactory
+      val g = TitanFactory.build()
+        .set("storage.backend", "cassandra")
+        .set("storage.hostname", "127.0.0.1")
+        .open()
 
-    success
+      val gs = GremlinScala(g)
+
+      val n0  = gs.addVertex(label = "n0",  properties = Map("name" -> "ORac"))
+      val n1  = gs.addVertex(label = "n1",  properties = Map("name" -> "OrSO"))
+      val n11 = gs.addVertex(label = "n11", properties = Map("name" -> "ele1"))
+      val n12 = gs.addVertex(label = "n12", properties = Map("name" -> "gal1"))
+
+      val e7 = n0.addEdge("e7", n11, Map.empty)
+      val e8 = n0.addEdge("e8", n12, Map.empty)
+      val e9 = n1.addEdge("e9", n0,  Map.empty)
+
+      (g, gs)
+    }
   }
 }
